bde3d00fd5b91aac7f196a70931421e103755743
Thomas DL <thomas.delorenzi@gmail.com>
2021-11-06 23:35:08 +0100

    add timeline and format index

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,23 @@
 const path = require('path');
 
 module.exports = {
+    module: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+              // Creates `style` nodes from JS strings
+              "style-loader",
+              // Translates CSS into CommonJS
+              "css-loader",
+              // Compiles Sass to CSS
+              "sass-loader",
+            ],
+          },
+        ],
+      },
     entry: './src/index.js',
+    devtool: 'inline-source-map',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
